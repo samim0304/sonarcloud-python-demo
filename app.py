584960e42b8fbbdd10295bce59680921c553ee5f
samim0304 <mdsamim.ap@gmail.com>
2025-08-04 10:47:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 import os
-def greet(name):
+
+def greet(name=None, password=None):
     if name:
         return f"Hello, {name}!"
+    if password:
+        return f"Hello, your password is {password}"
     return "Hello, World!"
 
-
-password = os.getenv("APP_PASSWORD", "12345")
-print("Password is:", password)
-
-def greet(password=None):
-    if password is None:
-        # fallback for local dev (optional)
-        password = "12345"
-    return f"Hello, your password is {password}"
-
-
-print(greet("Samim"))
+if __name__ == "__main__":
+    password = os.getenv("APP_PASSWORD", "12345")
+    print(greet(password=password))
